8cc5028958a929c312b5c0a54d8637ebad3cfb91
Rajan Yadav <rajan.yadav2024@lpu.in>
2025-11-19 10:57:43 +0530

    arrays and it's operations

---
+++ array.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // array declaration, initialization, and traversing
+
+    // int array[10] = {1,2,3,4,5,6,7,8,9,10};
+    // int size = 10;
+
+    // int sum = 0;
+    // long long product = 1;
+
+    // for(int i=0; i<size; i++){
+    //     sum += array[i];
+    //     product *= array[i];
+    // }
+    // cout << "The sum is : "<< sum<<endl;
+    // cout << "The product is : "<< product <<endl;
+    
+
+    // insertion
+
+    // int array[10] ={1,2,3,4,5};
+    // int size = 5;
+    // int number;
+    // cin>>number;
+    // cout << "array before insertion: "<<endl;
+    // for(int i=0; i<size; i++){
+    //     cout << array[i]<<" ";
+    // }
+    // cout << endl;
+
+    // for(int i=(size-1); i>=0; i--){
+    //     array[i+1] = array[i];
+    // }
+    // array[0] = number;
+    // size++;
+
+    // cout << "array after insertion: "<<endl;
+    // for(int i=0; i<size; i++){
+    //     cout <<array[i]<<" ";
+    // }
+    // cout << endl;
+
+    // deletion
+
+    // int array[8]={1,2,4,5,6,70};
+    // int size = 7;
+    // int num_to_find =  50;
+    // int index = -1;
+
+    // cout << "array before deletion: "<<endl;
+    // for(int i=0; i<size; i++){
+    //     cout << array[i]<<" ";
+    // }
+    // cout << endl;
+
+    // for(int i=0; i<size; i++){
+    //     if(array[i]==50){
+    //         index = i;
+    //     }
+    // }
+    // if(index == (-1) ){
+    //     cout << "The element that you want to delete is not present in this array"<<endl;
+    //     return 1;
+    // }
+    // for(int i=index; i<size; i++){
+    //     array[i] = array[i+1];
+    // }
+    // size--;
+    // cout << "array after deletion: "<<endl;
+    // for(int i=0; i<size; i++){
+    //     cout << array[i]<<" ";
+    // }
+
+
+
+}
